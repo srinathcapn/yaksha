a146e286daad6db361c006a4299ccdc68098561e
veema <veema.appiness@gmail.com>
2017-12-06 22:02:11 +0530

    added contact us functionality

---
+++ conn.php
@@ -0,0 +1,3 @@
+<?php
+$GLOBALS['link'] = mysqli_connect('localhost','root','root','yaksha')or die("Error Connecting to PHPmyadmin");
+?>
---
+++ functions.php
@@ -0,0 +1,54 @@
+<?php 
+include_once('conn.php');
+
+extract($GLOBALS);
+
+$action = '';
+	if (isset($_REQUEST['action'])) {
+	    $action = $_REQUEST['action'];
+	}
+
+	if ($action == 'insert') {
+	   extract($_POST);
+
+
+	    $query = 'INSERT INTO `contact_us` (`name`,`email_id`,`phone`,`service`,`date`,`company`)VALUES("' . $name . '","'. $email_id . '","' . $phone . '","' . $service . '",  "'.$date.'",  "'.$company.'")';
+	    $result = mysqli_query($link, $query) or die('Error in Query.' . mysqli_error($link));
+	    $req_id = mysqli_insert_id($link);
+
+	    $email_array = array('veema3008@gmail.com','bookings@yakshatravels.com');
+        
+		    for($e=0;$e<count($email_array);$e++)
+		    {
+		        $to  =  $email_array[$e];
+		        $subject = 'Contact Us Enquiry';
+		        // defineith \n
+		        $message = "<html>
+		                        <body>
+		                            <table>
+		                                <tr><td>Name</td><td>:</td><td>".$name."</td></tr> 
+		                                <tr><td>Phone Number</td><td>:</td><td>".$phone."</td></tr>
+		                                <tr><td>Email</td><td>:</td><td>".$email_id."</td></tr>
+		                                <tr><td>Company</td><td>:</td><td>".$company."</td></tr>
+		                                <tr><td>Vehicle</td><td>:</td><td>".$service."</td></tr>
+		                                <tr><td>Date</td><td>:</td><td>".$date."</td></tr>
+		                            </table>
+		                        </body>
+		                    </html>";   
+		        
+		        $from='veema@appinessworld.com'; 
+		        // define the headers we want passed. Note that they are separated with \r\n0
+		        $headers  = "From: " . strip_tags($from) . "\r\n";
+		        $headers .= "Reply-To: ". strip_tags($email_id) . "\r\n";
+		        $headers .= "MIME-Version: 1.0" . "\r\n";
+                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";     
+		        $mail_sent = mail($to, $subject, $message, $headers);
+
+		    } 
+		    
+	    echo json_encode($req_id);
+	  
+	    mysqli_close($link);
+	}
+
+?>
